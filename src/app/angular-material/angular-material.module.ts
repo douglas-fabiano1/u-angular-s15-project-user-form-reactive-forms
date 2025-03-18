59a35dd2d6f9549f427aa5a5b4ffd4828df77a0e
Douglas <contatodouglasfabiano1@gmail.com>
2025-03-18 18:14:55 -0300

    tabs component

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  imports: [MatButtonModule, MatCardModule],
-  exports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, MatTabsModule],
+  exports: [MatButtonModule, MatCardModule, MatTabsModule],
 })
 export class AngularMaterialModule {}
