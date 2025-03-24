8353014b6cbe1b29fc10c243b2547cd80c620fb3
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-03-24 17:18:19 -0300

    general informatios edit component div correction

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -5,9 +5,13 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule,],
-  exports: [MatButtonModule, MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule,],
+  imports: [MatButtonModule, MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule, MatRadioModule, MatDatepickerModule],
+  exports: [MatButtonModule, MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule, MatRadioModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
 })
 export class AngularMaterialModule { }
