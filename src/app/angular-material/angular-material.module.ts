10972b636dbb21cfae2c5274ceae25d3fd18f49d
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-03-24 15:30:15 -0300

    general informations edit component created

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
-  imports: [MatButtonModule, MatCardModule, MatTabsModule],
-  exports: [MatButtonModule, MatCardModule, MatTabsModule],
+  imports: [MatButtonModule, MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule,],
+  exports: [MatButtonModule, MatCardModule, MatTabsModule, MatFormFieldModule, MatInputModule, MatAutocompleteModule,],
 })
-export class AngularMaterialModule {}
+export class AngularMaterialModule { }
