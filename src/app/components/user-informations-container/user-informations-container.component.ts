cc7ac4dc305f65dffdd0d200033825cacb020bf8
Douglas <contatodouglasfabiano1@gmail.com>
2025-03-27 11:34:16 -0300

    loading list of countries

--- src/app/components/user-informations-container/user-informations-container.component.ts
+++ src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,26 +1,58 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { IUser } from '../../interfaces/user/user-interface';
 import { UserFormController } from './user-form-controller';
+import { CountriesService } from '../../services/countries.service';
+import { take } from 'rxjs';
+import { CountriesList } from '../../types/countries-list';
 
 @Component({
   selector: 'app-user-informations-container',
   standalone: false,
   templateUrl: './user-informations-container.component.html',
-  styleUrl: './user-informations-container.component.scss'
+  styleUrl: './user-informations-container.component.scss',
 })
-export class UserInformationsContainerComponent extends UserFormController implements OnChanges {
+export class UserInformationsContainerComponent
+  extends UserFormController
+  implements OnInit, OnChanges
+{
   currentTabIndex: number = 0;
+
+  countriesList: CountriesList = [];
+
+  private readonly _countriesService = inject(CountriesService);
 
   @Input({ required: true }) isInEditMode: boolean = false;
   @Input({ required: true }) userSelected: IUser = {} as IUser;
 
+  ngOnInit() {
+    this.getCountriesList();
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     this.currentTabIndex = 0;
 
-    const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
+    const HAS_USER_SELECTED =
+      changes['userSelected'] &&
+      Object.keys(changes['userSelected'].currentValue).length > 0;
 
     if (HAS_USER_SELECTED) {
       this.fulfillUserForm(this.userSelected);
     }
+  }
+
+  private getCountriesList() {
+    this._countriesService
+      .getCountries()
+      .pipe(take(1))
+      .subscribe((countriesList: CountriesList) => {
+        this.countriesList = countriesList;
+      });
   }
 }
