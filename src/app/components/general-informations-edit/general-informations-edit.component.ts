ae491feb2a8c2fb39d7c9fc01db5e90f75d16fcf
Douglas <contatodouglasfabiano1@gmail.com>
2025-03-27 10:26:02 -0300

    name and email validators

--- src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-general-informations-edit',
   standalone: false,
   templateUrl: './general-informations-edit.component.html',
-  styleUrl: './general-informations-edit.component.scss'
+  styleUrl: './general-informations-edit.component.scss',
 })
 export class GeneralInformationsEditComponent {
   @Input({ required: true }) userForm!: FormGroup;
 
-
+  get emailControl(): FormControl {
+    return this.userForm.get('generalInformations.email') as FormControl;
+  }
 }
--- src/app/components/user-informations-container/user-form-controller.ts
+++ src/app/components/user-informations-container/user-form-controller.ts
@@ -1,12 +1,14 @@
-import { inject } from "@angular/core";
-import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { IUser } from "../../interfaces/user/user-interface";
-import { PhoneList } from "../../types/phone-list";
-import { AddressList } from "../../types/address-list";
-import { DependentsList } from "../../types/dependents-list";
+import { inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IUser } from '../../interfaces/user/user-interface';
+import { PhoneList } from '../../types/phone-list';
+import { AddressList } from '../../types/address-list';
+import { DependentsList } from '../../types/dependents-list';
 
 export class UserFormController {
   userForm!: FormGroup;
+
+  private emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   private _fb = inject(FormBuilder);
 
@@ -60,35 +62,41 @@ export class UserFormController {
 
   private fulfillDependentsList(userDependentsList: DependentsList) {
     userDependentsList.forEach((dependent) => {
-      this.dependentsList.push(this._fb.group({
-        name: [dependent.name, Validators.required],
-        age: [dependent.age, Validators.required],
-        document: [dependent.document, Validators.required],
-      }));
-    })
+      this.dependentsList.push(
+        this._fb.group({
+          name: [dependent.name, Validators.required],
+          age: [dependent.age, Validators.required],
+          document: [dependent.document, Validators.required],
+        })
+      );
+    });
   }
 
   private fulfillAddressList(userAddressList: AddressList) {
     userAddressList.forEach((address) => {
-      this.addressList.push(this._fb.group({
-        type: [address.type, Validators.required],
-        street: [address.street, Validators.required],
-        complement: [address.complement, Validators.required],
-        country: [address.country, Validators.required],
-        state: [address.state, Validators.required],
-        city: [address.city, Validators.required],
-      }))
-    })
+      this.addressList.push(
+        this._fb.group({
+          type: [address.type, Validators.required],
+          street: [address.street, Validators.required],
+          complement: [address.complement, Validators.required],
+          country: [address.country, Validators.required],
+          state: [address.state, Validators.required],
+          city: [address.city, Validators.required],
+        })
+      );
+    });
   }
 
   private fulfillPhoneList(userPhoneList: PhoneList) {
     userPhoneList.forEach((phone) => {
-      this.phoneList.push(this._fb.group({
-        type: [phone.type, Validators.required],
-        areaCode: [phone.areaCode, Validators.required],
-        internationalCode: [phone.internationalCode, Validators.required],
-        number: [phone.number, Validators.required],
-      }));
+      this.phoneList.push(
+        this._fb.group({
+          type: [phone.type, Validators.required],
+          areaCode: [phone.areaCode, Validators.required],
+          internationalCode: [phone.internationalCode, Validators.required],
+          number: [phone.number, Validators.required],
+        })
+      );
     });
   }
 
@@ -100,7 +108,10 @@ export class UserFormController {
     this.userForm = this._fb.group({
       generalInformations: this._fb.group({
         name: ['', Validators.required],
-        email: ['', Validators.required],
+        email: [
+          '',
+          [Validators.required, Validators.pattern(this.emailPattern)],
+        ],
         country: ['', Validators.required],
         state: ['', Validators.required],
         maritalStatus: [null, Validators.required],
