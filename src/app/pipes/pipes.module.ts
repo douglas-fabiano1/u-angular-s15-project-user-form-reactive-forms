4ac8d1cde255a9f476fa652db79cd93cbca65222
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-03-29 16:48:42 -0300

    placeholder pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { MaritalStatusPipe } from './marital-status.pipe';
 import { CpfPipe } from './cpf.pipe';
 import { PhoneMaskPipe } from './phone-mask.pipe';
+import { PhonePlaceholderPipe } from './phone-placeholder.pipe';
 
 @NgModule({
-  declarations: [MaritalStatusPipe, CpfPipe, PhoneMaskPipe],
-  exports: [MaritalStatusPipe, CpfPipe, PhoneMaskPipe],
+  declarations: [MaritalStatusPipe, CpfPipe, PhoneMaskPipe, PhonePlaceholderPipe],
+  exports: [MaritalStatusPipe, CpfPipe, PhoneMaskPipe, PhonePlaceholderPipe],
 })
 export class PipesModule { }
