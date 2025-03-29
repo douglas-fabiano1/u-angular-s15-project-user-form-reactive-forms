ea370735fe4b3584d95f6a174a47b109be6f5aaa
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-03-29 15:53:35 -0300

    phone mask pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MaritalStatusPipe } from './marital-status.pipe';
 import { CpfPipe } from './cpf.pipe';
+import { PhoneMaskPipe } from './phone-mask.pipe';
 
 @NgModule({
-  declarations: [MaritalStatusPipe, CpfPipe],
-  exports: [MaritalStatusPipe, CpfPipe],
+  declarations: [MaritalStatusPipe, CpfPipe, PhoneMaskPipe],
+  exports: [MaritalStatusPipe, CpfPipe, PhoneMaskPipe],
 })
-export class PipesModule {}
+export class PipesModule { }
