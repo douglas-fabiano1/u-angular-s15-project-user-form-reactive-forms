892f9bc57f0d1b1f14a0ce15b2c7066660b253bc
Douglas <contatodouglasfabiano1@gmail.com>
2025-04-01 16:53:33 -0300

    rendering address list for edition

--- src/app/components/contact-informations-edit/components/address-list-edit/address-list-edit.component.ts
+++ src/app/components/contact-informations-edit/components/address-list-edit/address-list-edit.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-address-list-edit',
   standalone: false,
   templateUrl: './address-list-edit.component.html',
-  styleUrl: './address-list-edit.component.scss'
+  styleUrl: './address-list-edit.component.scss',
 })
 export class AddressListEditComponent {
+  @Input({ required: true }) userForm!: FormGroup;
 
+  get addressList(): FormArray {
+    return this.userForm.get('contactInformations.addressList') as FormArray;
+  }
 }
