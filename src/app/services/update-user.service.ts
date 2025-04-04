11bbacba5c68469ab120c958ce65155600c6af39
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-04-04 18:11:14 -0300

    service to save user changes

---
+++ src/app/services/update-user.service.ts
@@ -0,0 +1,15 @@
+import { IUser } from './../interfaces/user/user-interface';
+import { Injectable } from "@angular/core";
+import { map, Observable } from "rxjs";
+
+@Injectable({ providedIn: 'root', })
+export class UpdateUserService {
+  updateUser(newUser: IUser): Observable<IUser> {
+    return new Observable<{ status: number; body: IUser; }>((observer) => {
+      setTimeout(() => {
+        observer.next({ status: 200, body: structuredClone(newUser), });
+        observer.complete();
+      }, 500);
+    }).pipe(map((updateUserResponse) => updateUserResponse.body));
+  }
+}
