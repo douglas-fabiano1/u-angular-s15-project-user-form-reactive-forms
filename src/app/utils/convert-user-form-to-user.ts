5d1bf58c1edb8af046a747bceacf2f6bf535ec3b
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-04-07 18:34:42 -0300

    correction on numbers list

--- src/app/utils/convert-user-form-to-user.ts
+++ src/app/utils/convert-user-form-to-user.ts
@@ -34,12 +34,14 @@ const convertGeneraInformations = (generalInformations: IUserFormGeneralInformat
 }
 
 const convertPhoneList = (phoneList: IUserFormPhone[]): PhoneList => {
-  const newUserPhoneList: PhoneList = phoneList.map((phone) => ({
-    type: phone.type,
-    internationalCode: '+' + phone.number.substring(0, 2),
-    areaCode: phone.number.substring(2, 4),
-    number: formatNumber(phone.number.substring(4)),
-  }));
+  const newUserPhoneList: PhoneList = phoneList
+    .map((phone) => ({
+      type: phone.type,
+      internationalCode: '+' + phone.number.substring(0, 2),
+      areaCode: phone.number.substring(2, 4),
+      number: formatNumber(phone.number.substring(4)),
+    }))
+    .filter((phone) => phone.areaCode !== '');
 
   return newUserPhoneList;
 };
